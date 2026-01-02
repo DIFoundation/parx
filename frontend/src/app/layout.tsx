0c1919ee929a50e75cba3421503342d80f6bfffe
DIFoundation <adeniranibrahim165@gmail.com>
2026-01-02 04:46:30 +0100

    change font to inter

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { headers } from 'next/headers' // added
 import ContextProvider from '@/context'
 import Navbar from '@/components/Navbar'
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,7 +23,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.style} antialiased`}
+        className={`${inter.style} antialiased`}
       >
         <ContextProvider cookies={cookies}>
           <Navbar />
